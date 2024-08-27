5c5276b2e16625359622200ef2ebe5e67d4040d3
Nobre 81 <jnobr3c+git@gmail.com>
2024-08-27 00:51:10 -0300

    :construction: Criando o primeiro tipo - Aula 17

--- schema-query/projeto/index.js
+++ schema-query/projeto/index.js
@@ -3,10 +3,20 @@ const{ ApolloServer, gql } = require('apollo-server')
 const typeDefs = gql`
     scalar Date
 
+    type Usuario {
+        id: ID
+        nome: String
+        email: String
+        idade: Int
+        salario: Float
+        vip: Boolean
+    }
+
     # Pontos de entradas da sua API!
     type Query {
-        ola: String
-        horaAtual: Date
+        ola: String !
+        horaAtual: Date !
+        usuarioLogado: Usuario
     }
 `
 
@@ -18,6 +28,16 @@ const resolvers = {
         horaAtual() {
            // return `${new Date}`
            return new Date
+        },
+        usuarioLogado(){
+            return{
+                id: 1,
+                nome: 'Ana da Web',
+                email: 'anadaweb@email.com',
+                idade: 23,
+                salario: 1234.56,
+                vip: true
+            }
         }
     }
 }
