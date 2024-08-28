24e4daf027c59cf134614151ef490f063f7d3978
Nobre 81 <jnobr3c+git@gmail.com>
2024-08-28 00:54:47 -0300

    :construction: Enum - Aula 31

--- schema-query/projeto/data/db.js
+++ schema-query/projeto/data/db.js
@@ -8,19 +8,22 @@ const usuarios = [{
     nome: 'João Silva',
     email: 'jsilva@email.com',
     idade: 29,
-    perfil_id: 1
+    perfil_id: 1,
+    status: 'ATIVO'
 },{
     id: 2,
     nome: 'Rafael Junior',
     email: 'rafajun@email.com',
     idade: 31,
-    perfil_id: 2
+    perfil_id: 2,
+    status: 'INATIVO'
 },{
     id: 3,
     nome: 'Daniela Smith',
     email: 'danismi@email.com',
     idade: 24,
-    perfil_id: 1
+    perfil_id: 1,
+    status: 'BLOQUEADO'
 }]
 
 module.exports = { usuarios, perfis}
