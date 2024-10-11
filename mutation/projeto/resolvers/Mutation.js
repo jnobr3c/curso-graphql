9983e2ca54890e4c6d3ee99aac1d284aed4d37d5
Nobre 81 <jnobr3c+git@gmail.com>
2024-10-10 23:44:21 -0300

    :construction: Aula 39 Mutation para alterar Usuário

--- mutation/projeto/resolvers/Mutation.js
+++ mutation/projeto/resolvers/Mutation.js
@@ -26,5 +26,27 @@ module.exports = {
         if(i < 0) return null
         const excluidos = usuarios.splice(i, 1)
         return excluidos ? excluidos [0] : null
+    },
+    alterarUsuario(_, args){
+       const i = usuarios.findIndex(u => u.id === args.id)
+       if(i < 0) return null
+
+       usuarios[i].nome = args.nome
+       usuarios[i].email = args.email
+       
+       if(args.idade) {
+        usuarios[i].idade = args.idade
+       }
+       return usuarios[i]
+
+       //2º Possibilidade
+       
+       //const usuario = {
+        //...usuarios[i],
+        //...args
+       //}       
+
+       //usuarios.splice(i, 1, usuario)
+       //return usuario
     }
 }
