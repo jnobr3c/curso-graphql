ba97724ae91ac1390453d0e1d7ad44b1eeba5892
Nobre 81 <jnobr3c+github@gmail.com>
2025-01-17 20:52:54 -0300

    :construction: 67 - Desafio Banco de Dados & GraphQL - Resposta #01

--- banco-de-dados/desafio/resolvers/Type/Usuario.js
+++ banco-de-dados/desafio/resolvers/Type/Usuario.js
@@ -3,5 +3,13 @@ const db = require('../../config/db')
 module.exports = {
     async perfis(usuario) {
         // implementar (Assunto novo!)
+        //juntar a tabela perfis com a tabela usuarios_perfis 
+        //Será esta consulta >> SELECT P.* FROM PERFIS P, USUARIOS_PERFIS UP WHERE UP.PERFIL_ID = P.ID AND UP.USUARIO_ID = 3
+        return db('perfis')
+            .join(
+               'usuarios_perfis', 
+               'perfis.id', // coluna
+               'usuarios_perfis.perfil_id' //coluna
+            ).where({ usuario_id: usuario.id })
     }
 }
