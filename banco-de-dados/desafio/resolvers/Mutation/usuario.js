8731e7780a12fe78d91547b082c226e18806c8c1
Nobre 81 <jnobr3c+github@gmail.com>
2025-01-17 23:48:50 -0300

    :construction: 69 - Desafio Banco de Dados & GraphQL - Resposta #03

--- banco-de-dados/desafio/resolvers/Mutation/usuario.js
+++ banco-de-dados/desafio/resolvers/Mutation/usuario.js
@@ -1,8 +1,62 @@
 const db = require('../../config/db')
+const { perfil: obterPerfil, perfil } = require('../Query/perfil') //recuperar a função
 
 module.exports = {
     async novoUsuario(_, { dados }) {
-        // Implementar
+        try {
+            const idsPerfis = []
+            /*if(dados.perfis) {
+                for(perfilFiltro of dados.perfis) {
+                    const perfil = await obterPerfil(_, {
+                        filtro: {...perfilFiltro}
+                    })
+                }
+            }*/
+           //forma generica
+           if(dados.perfis) {
+            for(filtro of dados.perfis) {
+                const perfil = await obterPerfil(_, {
+                    filtro
+                })
+                if(perfil) idsPerfis.push(perfil.id)
+            } 
+        }
+
+            delete dados.perfis
+
+            const [ id ] = await db('usuarios')
+                .insert({ ...dados })
+                /*Desta forma não precisaria excluir delete dados.perfis por que esta selecionando exatamente o que preciso inserir na tabela usuarios
+                .insert({ 
+                    nome: dados.nome,
+                    email: dados.email, 
+                    senha: dados.senha, 
+                })*/
+
+            for(perfil_id of idsPerfis) {
+                await db('usuarios_perfis')
+                    .insert({ perfil_id, usuario_id: id })
+            }
+
+            return db('usuarios')
+                .where({ id }).first()
+        }catch(e) {
+            throw new Error(e.sqlMessage)
+        }
+
+        /* const [ id ] = await db('usuarios')
+                .insert( dados )
+
+            for(perfil_id of idsPerfis) {
+                await db('usuarios_perfis')
+                    .insert({ perfil_id, usuario_id: id })
+            }
+
+            return db('usuarios')
+                .where({ id }).first()
+        }catch(e) {
+            throw new Error(e) //Mostra os erro sem tratamento
+        } */
     },
     async excluirUsuario(_, { filtro }) {
         // Implementar
