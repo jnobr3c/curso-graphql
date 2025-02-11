16499ca4a29e3209eeac94f5b957ad8f81761452
Nobre 81 <jnobr3c+github@gmail.com>
2025-02-11 17:52:30 -0300

    :construction: 81 - Login de Usuário '(Autenticação JWT)'

--- projeto-final/backend/resolvers/Query/usuario.js
+++ projeto-final/backend/resolvers/Query/usuario.js
@@ -1,6 +1,26 @@
 const db = require('../../config/db')
+const bcrypt = require('bcrypt-nodejs')
+const { getUsuarioLogado } =  require('../comum/usuario')
 
 module.exports = {
+    async login(_, { dados }) {
+        const usuario =  await db('usuarios')
+            .where({ email: dados.email})
+            .first()
+
+        if(!usuario) {
+            throw new Error('Usuário/Senha inválido')
+        }
+
+        const saoIguais =  bcrypt.compareSync(dados.senha,
+            usuario.senha)
+
+        if(!saoIguais) {
+            throw new Error('Usuário/Senha inválido')
+        }
+
+        return getUsuarioLogado(usuario)
+    },
     usuarios() {
         return db('usuarios')
     },
